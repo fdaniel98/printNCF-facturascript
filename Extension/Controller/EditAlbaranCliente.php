2685faabb6bf0f68fe780b259247e544baaf626e
Felix Licien <felix.licien@elaniin.com>
2022-08-01 21:07:06 -0400

    change

---
+++ Extension/Controller/EditAlbaranCliente.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace FacturaScripts\Plugins\PrintTicket\Extension\Controller;
+
+use Closure;
+use FacturaScripts\Dinamic\Lib\AssetManager;
+
+/**
+ * @method addButton(string $string, string[] $array)
+ */
+class EditAlbaranCliente
+{
+    public function createViews(): Closure
+    {
+        return function () {
+            AssetManager::add('js', FS_ROUTE . '/Plugins/PrintNCF/Assets/JS/printNCF.js');
+
+            $this->addButton('main', [
+                'action' => 'printTicketNCF()',
+                'color' => 'info',
+                'icon' => 'fas fa-print',
+                'label' => 'Imprimir ticket NCF',
+                'type' => 'js'
+            ]);
+        };
+    }
+}
